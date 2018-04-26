f1388795d96f1990c1f0f6a2ab0edc4ac94f7572
MikulaJakub <mikula_jakub@centrum.sk>
2018-04-26 23:02:13 +0800

    Update mesh_2D.m

--- Matlab/mesh_2D.m
+++ Matlab/mesh_2D.m
@@ -1,3 +1,12 @@
+na=2
+nb=2
+
+element = zeros(4,1);
+kkk = 1;
+swch = 1;
+
+
+
 TFEM = [1,2,3,4,9,10,7,8
         3,4,5,6,11,12,9,10
         7,8,9,10,15,16,13,14
@@ -38,3 +47,65 @@ perm_dof1(find(perm_element==2)) = 7;
 perm_dof1(find(perm_element==3)) = 3;
 perm_dof1(find(perm_element==4)) = 1;
 perm_dof1 = perm_dof1';
+
+%find what kkk-th iteration to skip adding kkk, save into RR
+k=1
+for i=1:31
+if perm_element(i)~=perm_element(i+1) & perm_element(i)<perm_element(i+1)
+RR(k) = i;
+k=k+1;
+end
+end
+
+%-------------------------------------------------------------------------
+
+for j=1:nb
+for i=1:na
+
+element(:) = -1;
+ if (i>1 & j>1)
+ 	element(1) = (j-2)*(na-1) + i-1;
+ end
+ if (i<na & j>1)
+	element(2) = (j-2)*(na-1) + i;
+ end
+ if (i>1 & j<nb)
+ 	element(3) = (j-1)*(na-1) + i-1;
+ end
+ if (i<na & j<nb)
+	element(4) = (j-1)*(na-1) + i;
+ end
+
+for nxdof=1:2
+for dof=1:32
+
+if isempty(find([1,2,3,5,7,8,9,11,13,17,21,23,25,26,27,29,31,32]==dof))==0
+      info=0;
+end
+
+%for el=1:4
+	if (element(perm_element(dof))~=-1)
+		info = 1;	%element does exist
+	else
+	 	info = 0;	%element does not exist
+	end
+
+		if (isempty(find(dof==RR))==1 & info==1)		
+			%find the DOF which is added 
+				JA(kkk) = 2*((j-1+indx_j(dof))*na + i+indx_i(dof)) -1*swch
+			if swch == 0
+				swch = 1;
+			else
+				swch = 0;
+			end %if
+		kkk = kkk+1;
+		end %if
+	
+%end %el
+
+
+end %dof
+end %nxdof
+
+end %na
+end %nb
